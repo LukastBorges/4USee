75388cd7e909fe1ccbc7aa07d4c5d0e358de6658
Lucas Tarley Borges <lukasborges@outlook.com>
2018-04-08 13:50:39 -0300

    [014]: adjusts e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('for-you-see App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('4YouSee Challenge');
   });
 });
