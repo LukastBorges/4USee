8f9dc2639f7de41224c422a8ecaa8fef9f16477f
Lucas Tarley Borges <lukasborges@outlook.com>
2018-04-08 13:31:38 -0300

    [008]: adds material icon to view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
 
 
 import { AppComponent } from './components/app.component';
 import { DashboardService } from './services/dashboard.service';
+import { IconPipe } from './pipes/icon.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconPipe
   ],
   imports: [
     BrowserModule,
@@ -21,7 +24,8 @@ import { DashboardService } from './services/dashboard.service';
     MatCardModule,
     FlexLayoutModule,
     MatToolbarModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatIconModule
   ],
   providers: [
     HttpClientModule,
